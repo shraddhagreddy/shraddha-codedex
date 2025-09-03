6f7afed7bdee3a00c88d29e2a86a6b39af1f3d06
shraddhagreddy <shraddhagreddy@gmail.com>
2025-09-03 19:51:45 +0530

    Initial commit of my Codedex codes

---
+++ Hello_world/initials.py
@@ -0,0 +1,8 @@
+# Write code below 💖
+
+#I am trying to learn coding and im actually going to do it this time
+print("SSSS  RRRR")
+print("S     R  R")
+print("SSSS  RRRR")
+print("   S  R R")
+print("SSSS  R  R")
